e83b2640f059e1b238f4081e8148029500f873e7
Tinbite1 <80796613+Tinbite1@users.noreply.github.com>
2021-08-05 15:59:58 -0400

    Practice commit 1

---
+++ src/Tin_Practice/Practice.java
@@ -0,0 +1,31 @@
+package Tin_Practice;
+
+public class Practice {
+
+    public int amount;
+    public int amount2;
+
+    public int  plus(int num, int num2 ){
+        this.amount = num;
+        this.amount2 =num2;
+       return amount2 + amount;
+    }
+
+    public int  minus(int num, int num2 ){
+        this.amount = num;
+        this.amount2 =num2;
+        return amount2 + amount;
+    }
+    public int  multiply(int num, int num2 ){
+        this.amount = num;
+        this.amount2 =num2;
+        return amount2 + amount;
+    }
+
+    public int  divide(int num, int num2 ){
+        this.amount = num;
+        this.amount2 =num2;
+        return amount2 + amount;
+    }
+    
+}
